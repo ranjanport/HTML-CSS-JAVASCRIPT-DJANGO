49181e897ed910b14e50fa87c913c11ad3d7fef6
Aman Ranjan <amanranjanbharti@gmail.com>
2020-11-14 14:22:32 +0530

    added more app files

--- SERVER/tasks/views.py
+++ SERVER/tasks/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from django import forms
 tasks = ["foo", "", "hello"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label = "New Task") 
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html", {
         "tasks" : tasks
+    })
+def addtask(request, method = "POST"):
+    return render(request, "tasks/addtask.html",{
+        "form": NewTaskForm()
     })
